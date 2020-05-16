de0e7299084ffb411cb877e26fd051cd0b01908d
okayu <moka1988jp@gmail.com>
2020-05-16 15:36:26 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # pixi-mask-test
+
+## Run
+
+```bash
+npm install
+npm start
+# Available on localhost:8080
+```
+
+## Result
+
+| Base | Mask | Result |
+|------|------|--------|
+|<img src="docs/img/img1.png" width="200">|<img src="docs/mask/mask1.png" width="200">|<img src="result/result-mask1.png" width="200">|
+|<img src="docs/img/img1.png" width="200">|<img src="docs/mask/mask2.png" width="200">|<img src="result/result-mask2.png" width="200">|
+|<img src="docs/img/img1.png" width="200">|<img src="docs/mask/mask3.png" width="200">|<img src="result/result-mask3.png" width="200">|
+|<img src="docs/img/img1.png" width="200">|<img src="docs/mask/mask4.png" width="200">|<img src="result/result-mask4.png" width="200">|
---
+++ docs/script.js
@@ -0,0 +1,36 @@
+document.getElementById("version").innerText = `Pixi.js Version ${PIXI.VERSION}`;
+
+
+const app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  transparent: false
+});
+document.getElementById('canvas_wrapper').appendChild(app.view);
+
+const container = new PIXI.Container();
+app.stage.addChild(container);
+
+const img1 = document.getElementById("img1");
+const sprite = new PIXI.Sprite(PIXI.Texture.from(img1));
+sprite.anchor.set(0);
+sprite.x = 0;
+sprite.y = 0;
+container.addChild(sprite);
+
+const maskSprites = (() => {
+  maskMap = {};
+  ['mask1', 'mask2', 'mask3', 'mask4'].forEach((id) => {
+    const maskImg = document.getElementById(id);
+    const sprite = new PIXI.Sprite(PIXI.Texture.from(maskImg));
+    maskMap[id] = sprite;
+  });
+  return maskMap;
+})();
+container.mask = maskSprites['mask1'];
+
+$('.mask-btn').on('click', (e) => {
+  const $btn = $(e.currentTarget);
+  const maskId = $btn.data('mask');
+  container.mask = maskSprites[maskId];
+});
